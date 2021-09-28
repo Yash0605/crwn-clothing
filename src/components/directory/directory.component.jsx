084d258d128d77f08ebc145bc0b237dc3b137219
yash <hsay0605@gmail.com>
2021-09-28 20:48:03 +0530

    Added the changes to see the effect of routing in React

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,12 +47,10 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, title, imageUrl, size }) => (
+        {this.state.sections.map(({ id, ...otherMenuProps }) => (
           <MenuItem
             key={id}
-            title={title}
-            imageUrl={imageUrl}
-            size={size}
+            {...otherMenuProps}
           ></MenuItem>
         ))}
       </div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Directory from "../../components/directory/directory.component";
+
 import "./homepage.styles.scss";
+
+const HatsPage = props => (
+  <div>
+    <h1>This is Hats Page</h1>
+  </div>
+)
 
 const HomePage = (props) => (
   <div className="homepage">
-    <Directory></Directory>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Directory></Directory>
+        </Route>
+        <Route path="/shop/hats">
+          <HatsPage></HatsPage>
+        </Route>
+      </Switch>
+    </Router>
   </div>
 );
 
