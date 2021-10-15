27f92a48324799eef2b6a6dd1573359f608b9ca4
yash <hsay0605@gmail.com>
2021-10-15 14:04:55 +0530

    added the reselect package to make use of the concept of memoization while fetching our cart quantity and rendering the cart dropdown component

--- src/components/cart-Icon/cart-icon.component.jsx
+++ src/components/cart-Icon/cart-icon.component.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 import { toggleCartDropdown } from "../../redux/cart/cart.action";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 
 import { ReactComponent as ShoppingBag } from "../../assets/shopping-bag.svg";
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartDropdown}) => (
+const CartIcon = ({toggleCartDropdown, selectCartItemsCount}) => (
     <div className="cart-icon" onClick = {toggleCartDropdown}>
     <ShoppingBag className = "shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{selectCartItemsCount}</span>
     </div>
 );
 
@@ -17,4 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartDropdown : () => dispatch(toggleCartDropdown())
 });
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+const MapStateToProps = state => ({
+    selectCartItemsCount : selectCartItemsCount(state)
+})
+
+export default connect(MapStateToProps,mapDispatchToProps)(CartIcon);
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 
 import "./cart-dropdown.styles.scss";
 
@@ -14,8 +15,13 @@ const CartDropdown = ({cartItems}) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//   cartItems,
+// });
+
+//*using a selector instead of normally getting the cart items from the state because mapStateToProps method on components are called everytime the state of the app is changed, so even if the state changes because a new user signs in still the mapStateToProps will be called, now because of our structure this state is a new state post the state change so the CartDropdown component will be renedered, this can cause perf issues, so the sol we are using is reselect which uses the concept of memoization i.e caching. So now we are caching the cart property of the main state and thus the component will be rendered only if the cart property is changed, thus giving us perf benefits
+const mapStateToProps =state => ({
+  selectCartItems : selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
